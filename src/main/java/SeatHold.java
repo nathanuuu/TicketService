4f9278a9c0573d4c8dc19544681402ef6c1f0ee0
Nathan Wu <chen.wu@capitalone.com>
2017-06-05 09:27:43 -0400

    add interfaces and classes for program

---
+++ src/main/java/SeatHold.java
@@ -0,0 +1,2 @@
+public class SeatHold {
+}
---
+++ src/main/java/TicketServiceImpl.java
@@ -0,0 +1,14 @@
+public class TicketServiceImpl implements TicketService {
+
+    public int numSeatsAvailable() {
+        return 0;
+    }
+
+    public SeatHold findAndHoldSeats(int numSeats, String customerEmail) {
+        return null;
+    }
+
+    public String reserveSeats(int seatHoldId, String customerEmail) {
+        return "ok";
+    }
+}
