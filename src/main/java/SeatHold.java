d3a4df1e331f00ef9ae36ee4db5bc60fcf3a7121
Nathan Wu <mail.nathan.wu@gmail.com>
2017-06-06 19:24:58 -0400

    add new features timestamp and failure message to SeatHold object

--- src/main/java/SeatHold.java
+++ src/main/java/SeatHold.java
@@ -1,15 +1,18 @@
 public class SeatHold {
 
     boolean success;
+    String message = "Seat hold successful!";
     String id;
     int numSeats;
     String customerEmail;
+    long time;
 
-    public SeatHold(boolean success) throws Exception {
+    public SeatHold(boolean success, String message) throws Exception {
         if (success != false) {
             throw new Exception("Method can only be called with false value.");
         }
         this.success = success;
+        this.message = message;
     }
 
     public SeatHold(boolean success, String id, int numSeats, String customerEmail) throws Exception {
@@ -20,6 +23,7 @@ public class SeatHold {
         this.id = id;
         this.numSeats = numSeats;
         this.customerEmail = customerEmail;
+        this.time = System.currentTimeMillis();
     }
 
     public boolean getSuccess() {
