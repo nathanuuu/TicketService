dc487b80444e1800cae5e70c55fe36bb45d089ba
Nathan Wu <mail.nathan.wu@gmail.com>
2017-06-06 18:17:50 -0400

    create SeatHold object

--- src/main/java/SeatHold.java
+++ src/main/java/SeatHold.java
@@ -1,2 +1,40 @@
 public class SeatHold {
+
+    boolean success;
+    String id;
+    int numSeats;
+    String customerEmail;
+
+    public SeatHold(boolean success) throws Exception {
+        if (success != false) {
+            throw new Exception("Method can only be called with false value.");
+        }
+        this.success = success;
+    }
+
+    public SeatHold(boolean success, String id, int numSeats, String customerEmail) throws Exception {
+        if (success != false) {
+            throw new Exception("Method can only be called with true value.");
+        }
+        this.success = success;
+        this.id = id;
+        this.numSeats = numSeats;
+        this.customerEmail = customerEmail;
+    }
+
+    public boolean getSuccess() {
+        return success;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public int getNumSeats() {
+        return numSeats;
+    }
+
+    public String getCustomerEmail() {
+        return customerEmail;
+    }
 }
